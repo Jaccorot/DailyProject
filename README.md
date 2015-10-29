b813d414e13622bbc0ec04015b4ac068a5d96466
Jaccorot <caozijun007@163.com>
2015-10-29 21:20:36 +0800

    update todolist

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Daily Practises
 
 To do lists:
 
-1.A website base on django
+1.A website base on django:TodoList
 
 2.A spider for one lottery page
 
---
+++ ToDo/todolist/models.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+from django.db import models
+import django.utils.timezone as timezone
+
+
+class BusinessList(models.Model):
+    name = models.CharField('名称', max_length=50)
+    title = models.CharField('标题', max_length=100)
+    content = models.CharField('内容', max_length=1000)
+    comments = models.CharField('评论', max_length=1000, null=True)
+    plan_start_time = models.DateTimeField('计划开始时间', default=timezone.now())
+    plan_end_time = models.DateTimeField('计划完成时间', null=True)
+    plan_duration = models.DateTimeField('计划时长', null=True)
+    create_time = models.DateTimeField('创建时间', auto_now=True)
+    finish_time = models.DateTimeField('完成时间', null=True)
+    latest_update_time = models.DateTimeField('状态更新时间', default=timezone.now())
+
+    def __unicode__(self):
+        return self.name + ":" + self.title
+
+    def take_time(self):
+        return self.finish_time - self.create_time if self.finish_time else None
+
+    def finish_status(self):
+        return 1 if self.finish_time else 0
+
+# class BusinessDetail(models.Model):
+#     businesslist = models.ForeignKey(BusinessList)
+#     title = models.CharField(max_length=100)
+#     content = models.CharField(max_length=1000)
+#     comments = models.CharField(max_lenth=1000)
+#     plan_start_time = models.DateTimeField('计划开始时间')
+#     plan_end_time = models.DateTimeField('计划完成时间')
+#     plan_duration = models.DateTimeField('计划时长')
+#     create_time = models.DateTimeField('创建时间', auto_now=True)
+#     finish_time = models.DateTimeField('完成时间')
+#     take_time = models.DateTimeField('完成耗时')
+#     latest_update_time = models.DateTimeField('状态更新时间')
