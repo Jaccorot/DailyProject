9361546f8ab63d1fbf4fc3bde660cf7c35752f8d
Jaccorot <caozijun007@163.com>
2015-11-02 20:12:38 +0800

    add test files

---
+++ ToDo/test/functional_tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Todo', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
