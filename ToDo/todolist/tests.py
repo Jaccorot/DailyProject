82250f370980019ddb5c6eaf3954b70ec48bdbd3
Jaccorot <caozijun007@163.com>
2015-11-03 16:45:13 +0800

    able to make multy issues

--- ToDo/todolist/tests.py
+++ ToDo/todolist/tests.py
@@ -15,10 +15,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string(
-            'index.html',
-            {'new_item_text': 'A new list item'}
-        )
+        expected_html = render_to_string('index.html')
         self.assertEqual(response.content.decode(), expected_html)
         self.assertTrue(response.content.strip().startswith(b'<!DOCTYPE html>'))
         self.assertIn(b'<title>Todo List</title>', response.content)
@@ -28,9 +25,36 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
-
         response = home_page(request)
-        self.assertIn('A new list item', response.content.decode())
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 
--- ToDo/todolist/views.py
+++ ToDo/todolist/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.template import RequestContext
 from django.http import HttpResponse
 
+from todolist.models import Item
+
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
+        # new_item_text = request.POST.get('item_text', '')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+
     return render_to_response('index.html', locals(), context_instance=RequestContext(request))
